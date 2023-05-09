2a9ae7d1c9e3e01a7e0770b8b0668d3434f9c841
ronnie.gregersen@devoteam.com <ronnie.gregersen@devoteam.com>
2023-05-09 14:19:20 +0000

    Finished model

--- models/marts/customer_insights.sql
+++ models/marts/customer_insights.sql
@@ -11,6 +11,15 @@ payments as (
     select * from {{ref("stg_payments")}}
 )
 
-select o.*, c.*, p.* from orders o
+select o.USER_ID,
+o.ORDER_DATE,
+o.STATUS,
+c.FIRST_NAME,
+c.LAST_NAME,
+p.paymentmethod as PAYMENT_METHOD,
+p.status as PAYMENT_STATUS,
+p.amount as AMOUNT
+
+from orders o
 join customers c on c.ID = o.USER_ID
 join payments p on p.orderid = o.ID
